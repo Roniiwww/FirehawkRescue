8f8ca23d2d8ee61e357f28c9e6e4d0eac1b9621e
Roniiwww <ronosmani29@gmail.com>
2025-11-02 13:03:20 +0100

    connect backend routes to express server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { registerRoutes } from "./dist/server/routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+registerRoutes(app);
 
 app.use(express.static(path.join(__dirname, "dist/public")));
 
